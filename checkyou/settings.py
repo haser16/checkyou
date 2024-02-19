58eb9c8d65ab93035ed345c067dfff6c3287e2ae
Haser <deskatihonov@gmail.com>
2024-02-19 23:00:18 +0300

    Fix nex Bugs

--- checkyou/settings.py
+++ checkyou/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
 from os import path
 
@@ -24,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*_6@h!8*t*jw0u$*jm+rsga1n!+c&$ly@m^q4a$dsmpa*qt+dk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['haser.pythonanywhere.com']
 
 
 # Application definition
@@ -123,13 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-        BASE_DIR / 'static',
-)
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 
 # Media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = (BASE_DIR / 'media')
+MEDIA_ROOT = path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
